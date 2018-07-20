a508c44670be999e2a463f6b46efd1cb7641b229
Rei Lin <temisrei@gmail.com>
2018-07-20 14:23:23 +0800

    Deploying to Heroku after remove gitignore

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  line: {
+    channelSecret: '67d5c0f9a8c28203cf942334e9abb5ed',
+    accessToken: 'ztKNFFyxtsNL8ArXfEH1q4PpFumQt80t1njz+6ydpincwgcqMaE/sYKNFFiZyyn7jYZJwHAomk87URrTiMbqxk6f3PbbP7l6KtCA7uXlhPrhK1zYnh2uSXk+dmeote+BBCg0QgbGaKidh64uf+2yvAdB04t89/1O/w1cDnyilFU='
+  },
+  messenger: {
+    accessToken: '',
+    appSecret: '',
+    verifyToken: '',
+  },
+  telegram: {
+    accessToken: '',
+  },
+  olami: {
+    appKey: '0251abfcff3a41ec8ae569c5777079f7',
+    appSectet: '76cd5b31323842b4b28be958a5500166'
+  },
+  kkbox: {
+    id: '31529797c5e649e58a605072798de208',
+    secret: '47375425101307cc5824b68aa8da1831'
+  }
+}
