d4783fce0c40f731a64410832005559bbd7b9f85
Rei Lin <temisrei@gmail.com>
2018-07-20 12:58:12 +0800

    finish NLU part

--- src/nlp/Olami.js
+++ src/nlp/Olami.js
@@ -1,6 +1,7 @@
 const config = require('../../config')
 const axios = require('axios')
 const md5 = require('md5')
+const KKBOX = require('../api/KKBOX')
 
 class Olami {
   constructor(appKey = config.olami.appKey, appSecret = config.olami.appSectet, inputType = 1) {
@@ -37,6 +38,7 @@ class Olami {
     const type = nli.type
     const desc = nli.desc_obj
     const data = nli.data_obj
+    const semantic = nli.semantic
 
     function handleSelectionType(desc) {
       const descType = desc.type
@@ -45,29 +47,63 @@ class Olami {
         case 'news':
           return desc.result + '\n\n' + data.map((el, index) => index + 1 + '. ' + el.title).join('\n')
         case 'poem':
-          return // TODO
+          return desc.result + '\n\n' + data.map((el, index) => index + 1 + '. ' + el.poem_name).join('\n')
         case 'cooking':
-          return // TODO
+          return desc.result + '\n\n' + data.map((el, index) => index + 1 + '. ' + el.name).join('\n')
         default:
           return '對不起，你說的我還不懂，能換個說法嗎？'
       }
     }
 
+    async function handleMusicKKBOXType(semantic) {
+      function getKeyWord(semantic, dataType) {
+        function getSlotValueByName(slotName) {
+          return semantic.slots.filter(slot => slot.name === slotName)[0].value
+        }
+
+        switch (dataType) {
+          case 'artist':
+            return getSlotValueByName('artist_name')
+          case 'album':
+            return getSlotValueByName('album_name')
+          case 'track':
+            return getSlotValueByName('track_name')
+          case 'playlist':
+            return getSlotValueByName('keyword')
+        }
+      }
+
+      const dataType = semantic.modifier[0].split('_')[2]
+      const keyWord = getKeyWord(semantic, dataType)
+
+      const api = await KKBOX.init()
+      const data = await api
+        .searchFetcher
+        .setSearchCriteria(keyWord, dataType)
+        .fetchSearchResult()
+        .then(response => {
+          return response.data[dataType + 's'].data[0].url
+        })
+      return data
+    }
+
     switch (type) {
       case 'kkbox':
         return (data.length > 0) ? data[0].url : desc.result
-      case 'ds':
-        return desc.result + '\n請用 /help 指令看看我能怎麼幫助您'
-      case 'selection':
-        return handleSelectionType(desc)
-      case 'news':
-        return data[0].detail
       case 'baike':
         return data[0].description
+      case 'news':
+        return data[0].detail
       case 'joke':
-        return // TODO
+        return data[0].content
       case 'cooking':
-        return // TODO
+        return data[0].content
+      case 'selection':
+        return handleSelectionType(desc)
+      case 'ds':
+        return desc.result + '\n請用 /help 指令看看我能怎麼幫助您'
+      case 'music_kkbox':
+        return handleMusicKKBOXType(semantic[0])
       default:
         return desc.result
     }
