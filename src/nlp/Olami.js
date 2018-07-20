e4148672679e0b52b837813216f7f1babd9a3cf8
Rei Lin <temisrei@gmail.com>
2018-07-20 11:17:29 +0800

    modify Olami.js to add intentDetection

--- src/nlp/Olami.js
+++ src/nlp/Olami.js
@@ -28,8 +28,49 @@ class Olami {
         })
       }
     }).then(response => {
-      return response.data.data.nli[0].desc_obj.result
+      const nli = response.data.data.nli[0];
+      return this._intentDetection(nli)
     })
+  }
+
+  _intentDetection(nli) {
+    const type = nli.type
+    const desc = nli.desc_obj
+    const data = nli.data_obj
+
+    function handleSelectionType(desc) {
+      const descType = desc.type
+
+      switch (descType) {
+        case 'news':
+          return desc.result + '\n\n' + data.map((el, index) => index + 1 + '. ' + el.title).join('\n')
+        case 'poem':
+          return // TODO
+        case 'cooking':
+          return // TODO
+        default:
+          return '對不起，你說的我還不懂，能換個說法嗎？'
+      }
+    }
+
+    switch (type) {
+      case 'kkbox':
+        return (data.length > 0) ? data[0].url : desc.result
+      case 'ds':
+        return desc.result + '\n請用 /help 指令看看我能怎麼幫助您'
+      case 'selection':
+        return handleSelectionType(desc)
+      case 'news':
+        return data[0].detail
+      case 'baike':
+        return data[0].description
+      case 'joke':
+        return // TODO
+      case 'cooking':
+        return // TODO
+      default:
+        return desc.result
+    }
   }
 }
 
